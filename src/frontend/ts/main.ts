ab70195a4c89775991d7db60e9a27a8239c368dc
Your Name <you@example.com>
2024-08-26 21:32:11 -0300

    clase 2

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -3,4 +3,9 @@ function SayHello(){
     let current_value = document.getElementById("textarea_1") as HTMLInputElement;
     let new_value = "Hello world!!!" + "\n" + current_value.value;
     document.getElementById("textarea_1").innerHTML = new_value;
+
 }
+
+window.addEventListener("load",  ()=> {
+    
+});
